ec3a3434437db721b1fde5e2bd597b51975863c1
Ira Horecka <ira89@icloud.com>
2020-04-15 21:02:44 -0700

    Tutorial 3

--- ira_django_project/blog/views.py
+++ ira_django_project/blog/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+
+posts = [
+    {
+        "author": 'IraH',
+        "title": 'Blog Post 1',
+        "content": 'First post content',
+        "date_posted": 'April 14, 2020'  # usually a datetime obj
+    },
+    {
+        "author": 'GeneH',
+        "title": 'Blog Post 2',
+        "content": 'Second post content',
+        "date_posted": 'April 15, 2020'  # usually a datetime obj
+    }
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    title = {'title': 'About'}
+    return render(request, 'blog/about.html', title)
+
