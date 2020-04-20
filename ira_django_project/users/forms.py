cc1c80f63bf3b889f968afcc18f4b4c047f9bf7a
Ira Horecka <ira89@icloud.com>
2020-04-20 00:25:27 -0700

    Tutorial 9

--- ira_django_project/users/forms.py
+++ ira_django_project/users/forms.py
@@ -2,6 +2,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms  import UserCreationForm
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()  # default == required=True
@@ -9,3 +10,17 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']  # password1 is init password, password2 is confirmation
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
--- ira_django_project/users/models.py
+++ ira_django_project/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -8,5 +9,15 @@ class Profile(models.Model):
     def __str__(self):
         return f"{self.user.username} profile"
     
+    def save(self):
+        # method that is run after our model is saved -- override default save method
+        super().save()  # run the save method of our parent save method (call super)
 
-# Create your models here.
+        img = Image.open(self.image.path)
+        print('hit')
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)        
+            img.save(self.image.path)
+
