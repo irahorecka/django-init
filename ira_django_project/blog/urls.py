f5578e1d38a08cf6705f3112eba920b193717545
Ira Horecka <ira89@icloud.com>
2020-04-21 22:58:44 -0700

    Tutorial 10

--- ira_django_project/blog/urls.py
+++ ira_django_project/blog/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView
+)
 from . import views
 
 urlpatterns = [
     #  our blog.urls will handle the home (i.e. /blog) and home/about (i.e. /blog/about) urls.
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),  # pk = primary key, int is the type
+    path('post/new/', PostCreateView.as_view(), name='post-create'),  # default is <model>_form.html, i.e. post_form.html
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='blog-about'),
 ]
--- ira_django_project/blog/views.py
+++ ira_django_project/blog/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 from .models import Post
 
 
@@ -7,6 +15,66 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    # we need to create a var called model
+    model = Post
+    template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'  # i.e. posts == context key in our home func
+    ordering = ['-date_posted']  # goes oldest to newest -- add a '-' in front to reverse this order
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    # override the form valid method
+    
+    def form_valid(self, form):
+        # set author of form before submission
+        form.instance.author = self.request.user
+        # validate form by running the form_valid method on our parent class
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    # override the form valid method
+    
+    def form_valid(self, form):
+        # set author of form before submission
+        form.instance.author = self.request.user
+        # validate form by running the form_valid method on our parent class
+        return super().form_valid(form)
+
+    def test_func(self):
+        """UserPassesTestMixin will run this to check for correct
+        conditions, i.e. correct author"""
+        post = self.get_object()  # get post we are currently trying to update
+        # check current user is author of post
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        """UserPassesTestMixin will run this to check for correct
+        conditions, i.e. correct author"""
+        post = self.get_object()  # get post we are currently trying to update
+        # check current user is author of post
+        if self.request.user == post.author:
+            return True
+        return False
+
 
 
 def about(request):
