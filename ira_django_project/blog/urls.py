f94fe3d90c3987dac50080d6bbb1cc539407e4f4
Ira Horecka <ira89@icloud.com>
2020-04-23 12:43:36 -0700

    Tutorial 11

--- ira_django_project/blog/urls.py
+++ ira_django_project/blog/urls.py
@@ -4,13 +4,15 @@ from .views import (
     PostDetailView,
     PostCreateView,
     PostUpdateView,
-    PostDeleteView
+    PostDeleteView,
+    UserPostListView
 )
 from . import views
 
 urlpatterns = [
     #  our blog.urls will handle the home (i.e. /blog) and home/about (i.e. /blog/about) urls.
     path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-post'),  # username with string dtype and name it 'user-post'
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),  # pk = primary key, int is the type
     path('post/new/', PostCreateView.as_view(), name='post-create'),  # default is <model>_form.html, i.e. post_form.html
     path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
--- ira_django_project/blog/views.py
+++ ira_django_project/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 from django.views.generic import (
     ListView,
     DetailView,
@@ -23,6 +24,24 @@ class PostListView(ListView):
     template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'  # i.e. posts == context key in our home func
     ordering = ['-date_posted']  # goes oldest to newest -- add a '-' in front to reverse this order
+    paginate_by = 5  # no. posts per page
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    # ordering = ['-date_posted']
+    paginate_by = 5
+
+    def get_queryset(self):
+        """get the user associated with the username (get from url)
+        if user does not exist, return 404 page does not exist"""
+        user = get_object_or_404(User, username=self.kwargs.get('username'))  # get username from the parameter in the url
+        # we are overriding the query the list view is making, we are overriding ['-date_posted'] above -- move below like so
+        return Post.objects.filter(author=user).order_by('-date_posted')  # limit posts on page to this filter
+
+        
 
 
 class PostDetailView(DetailView):
